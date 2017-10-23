c8b50cad5139b3df1b81f1d6139b46d1c268f9dd
Nikita Vilunov <nikitaoryol@gmail.com>
2017-10-24 01:00:05 +0300

    Interop with Rust

---
+++ MessagerClient/Compressor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MessagerClient
+{
+    public static class Compressor
+    {
+        public static byte[] Compress(byte[] input)
+        {
+            unsafe
+            {
+                fixed (byte* p = input)
+                {
+                    Arr arr = BasicRaw((IntPtr) p, (UIntPtr) input.Length);
+                    byte[] output = new byte[(int) arr.len];
+                    Marshal.Copy(arr.ptr, output, 0, (int) arr.len);
+                    Free(arr);
+                    return output;
+                }
+            }
+        }
+    
+        [DllImport("libcompressors", EntryPoint="basic", ExactSpelling = true)]
+        private static extern Arr BasicRaw(IntPtr arr, UIntPtr len);
+        
+        [DllImport("libcompressors", EntryPoint="drop", ExactSpelling = true)]
+        private static extern void Free(Arr arr);
+    
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Arr
+        {
+            public IntPtr ptr;
+            public UIntPtr len;
+            private readonly UIntPtr cap;
+        }
+    }
+}
