691f6972cd764cedc464c1e63e4bf349cb63a0bd
Nitrogen13 <ilgizar_ru@mail.ru>
2017-10-23 13:30:17 +0300

    v 0.1
    Interface
    Some good things

---
+++ MessagerClient/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿using System.Windows.Forms;
+
+namespace MessagerClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Send = new System.Windows.Forms.Button();
+            this.History = new System.Windows.Forms.RichTextBox();
+            this.Message = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Send.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Send.Font = new System.Drawing.Font("Franklin Gothic Medium", 16F, System.Drawing.FontStyle.Bold);
+            this.Send.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Send.Location = new System.Drawing.Point(849, 338);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(155, 93);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = false;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // History
+            // 
+            this.History.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.History.Location = new System.Drawing.Point(13, 12);
+            this.History.Name = "History";
+            this.History.ReadOnly = true;
+            this.History.Size = new System.Drawing.Size(991, 308);
+            this.History.TabIndex = 2;
+            this.History.Text = "";
+            this.History.TextChanged += new System.EventHandler(this.History_TextChanged);
+            // 
+            // Message
+            // 
+            this.Message.BackColor = System.Drawing.SystemColors.Window;
+            this.Message.EnableAutoDragDrop = true;
+            this.Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Message.Location = new System.Drawing.Point(13, 338);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(818, 93);
+            this.Message.TabIndex = 3;
+            this.Message.Tag = "";
+            this.Message.Text = "";
+            this.Message.DragEnter += new DragEventHandler(FilesListBox_DragEnter);
+            this.Message.DragDrop += new DragEventHandler(FilesListBox_DragDrop);
+            // 
+            // Client
+            // 
+            this.AcceptButton = this.Send;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1017, 453);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.Send);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.RichTextBox History;
+        private System.Windows.Forms.RichTextBox Message;
+    }
+}
+
---
+++ MessagerClient/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessagerClient
+{
+    public partial class Client : Form
+    {
+        private String Name;
+        private String FilesListBox = "";
+
+        public Client()
+        {
+            InitializeComponent();
+            Name = "%name%";
+        }
+
+        void History_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            if (FilesListBox != "")
+                History.AppendText(DateTime.Now.ToLongTimeString() + "  " + Name + ": " + FilesListBox);
+            FilesListBox = "";
+
+            /*History.SelectionFont = new Font(History.SelectionFont, FontStyle.Bold);
+            History.AppendText(DateTime.Now.ToLongTimeString() + "\t" + Name + ": ");
+            History.SelectionFont = new Font(History.SelectionFont, FontStyle.Regular);
+            History.AppendText(Message.Text + "\n");*/
+            if (Message.Text != "")
+                History.AppendText(DateTime.Now.ToLongTimeString() + "  " + Name + ": " + Message.Text + "\n");
+            History.ScrollToCaret();
+            Message.Clear();
+        }
+
+        ////При нажате Enter нажимается кнопка
+        //private void Client_KeyUp(object sender, KeyPressEventArgs e)
+        //{
+        //    if (e.KeyChar == (char)Keys.Enter)
+        //    {
+        //        Send.PerformClick();
+        //    }
+        //}
+
+        private void FilesListBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void FilesListBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files)
+            {
+                FilesListBox += file + "\n";
+            }
+        }
+    }
+}
