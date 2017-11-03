c0b73177a63701d559ff0dac9fc3135dbda8ce48
rumblingturtle <edzaljaev@gmail.com>
2017-11-03 23:17:37 +0300

    Zaebis! Faili teper peredayutsya vsem
    V padlu v jopu ebatsya i golodnim ostatsya

--- MessagerClient/ClientForm.cs
+++ MessagerClient/ClientForm.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace MessagerClient
 {
@@ -11,16 +12,18 @@ namespace MessagerClient
     {
         class ChatMessage
         {
-            public ChatMessage(String ntype,String nname,String ndate, String ntext)
+            public ChatMessage(String ntype, String nname, String nfilename, String ndate, String ntext)
             {
                 type = ntype;
                 name = nname;
+                filename = nfilename;
                 date = ndate;
                 text = ntext;
             }
 
             public String type { get; set; }
             public String name { get; set; }
+            public String filename { get; set; }
             public String date { get; set; }
             public String text { get; set; }
         }
@@ -29,12 +32,14 @@ namespace MessagerClient
         private String[] FilesListBox = new String[0];
         private TcpClient client;
         private List List;
+        public const string SERVER_IP = "localhost";
+        public const int SERVER_PORT = 8080;
 
         public Client()
         {
             // закоментил подключение к серверу
-            //client = new TcpClient();
-            //client.Connect("localhost", 8080);
+            client = new TcpClient();
+            client.Connect(SERVER_IP, SERVER_PORT);
             InitializeComponent();
             this.Enabled = false;
             Welcome Welcome = new Welcome();
@@ -60,54 +65,30 @@ namespace MessagerClient
 
         private void Send_Click(object sender, EventArgs e)
         {
-            if (FilesListBox.Length != 0)
-            {
-                for (int i = 0; i < FilesListBox.Length; i++)
-                {
-                    List.AddFileToList(FilesListBox[i]);
-                }
-                History.AppendText(DateTime.Now.ToLongTimeString() + "  " + Name + ": Send these files to server: ");
-                for (int i = 0; i < FilesListBox.Length; i++)
-                    History.AppendText(FilesListBox[i]);
-                History.AppendText("\n");
-            }
-            
+            sendFiles();
             FilesListBox = new String[0];
             /*History.SelectionFont = new Font(History.SelectionFont, FontStyle.Bold);
             History.AppendText(DateTime.Now.ToLongTimeString() + "\t" + Name + ": ");
             History.SelectionFont = new Font(History.SelectionFont, FontStyle.Regular);
             History.AppendText(Message.Text + "\n");*/
-            if (Message.Text != "")
-                History.AppendText(DateTime.Now.ToLongTimeString() + "  " + Name + ": " + Message.Text + "\n");
-            History.ScrollToCaret();
 
             // for compression testing
             //byte[] arr = Compressor.Compress(new byte[] { 1, 3, 3, 7 });
             //foreach(var n in arr)
             //History.AppendText(n.ToString());
 
-
-            try
+            //Нахуй пустые сообщения
+            if (!String.IsNullOrWhiteSpace(Message.Text))
             {
-                NetworkStream stream = client.GetStream();
-
-                String messg = JsonConvert.SerializeObject(new ChatMessage("Text",Name, DateTime.Now.ToLongTimeString(),Message.Text));
+                History.AppendText(DateTime.Now.ToLongTimeString() + "  " + Name + ": " + Message.Text + "\n");
+                History.ScrollToCaret();
+                String messg = JsonConvert.SerializeObject(new ChatMessage("Text", Name, "", DateTime.Now.ToLongTimeString(), Message.Text));
                 byte[] data = System.Text.Encoding.UTF8.GetBytes(messg);
-                stream.Write(data, 0, data.Length);
-
-                stream.Flush();
+                sendBytes(data);
             }
 
-            catch (SocketException ex)
-            {
-                Console.WriteLine("SocketException: {0}", ex);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception: {0}", ex.Message);
-            }
 
-        Message.Clear();
+            Message.Clear();
         }
 
         private void ServerSays()
@@ -118,16 +99,21 @@ namespace MessagerClient
                 Application.DoEvents();
                 try
                 {
+                    if (client.Connected == false)
+                    {
+                        client = new TcpClient();
+                        client.Connect(SERVER_IP, SERVER_PORT);
+                    }
+
                     NetworkStream stream = client.GetStream();
 
-                    
                     byte[] data = new byte[256];
                     while (stream.DataAvailable)
                     {
                         int bytes = stream.Read(data, 0, data.Length);
-                        messg+=Encoding.UTF8.GetString(data, 0, bytes).ToString();
+                        messg += Encoding.UTF8.GetString(data, 0, bytes).ToString();
                     }
-                
+
                     stream.Flush();
                 }
                 catch (SocketException ex)
@@ -141,11 +127,26 @@ namespace MessagerClient
 
                 ChatMessage message = JsonConvert.DeserializeObject<ChatMessage>(messg);
 
-                if (messg!="" && message.type=="Text" && message.name!=Name)
-                {  
+                if (messg != "" && message.type == "Text" && message.name != Name)
+                {
                     History.AppendText(message.date + "  " + message.name + ": " + message.text + "\n");
                     History.ScrollToCaret();
                     History.Refresh();
+                }
+
+                //Оповещение о загруженном файле для всех клиентов
+                if (messg != "" && message.type == "Notification")
+                {
+                    History.AppendText(message.text);
+                    History.ScrollToCaret();
+                    History.Refresh();
+                }
+
+                //Ответ сервера на запрос о файле
+                if (messg != "" && message.type == "File")
+                {
+                    History.AppendText(messg);
+                    File.WriteAllBytes(message.filename, System.Convert.FromBase64String(message.text));
                 }
             }
         }
@@ -171,7 +172,7 @@ namespace MessagerClient
         {
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
             FilesListBox = new String[files.Length];
-            for(int i = 0; i < files.Length; i++)
+            for (int i = 0; i < files.Length; i++)
             {
                 FilesListBox[i] = files[i];
             }
@@ -187,6 +188,63 @@ namespace MessagerClient
                 Application.DoEvents();
             };
             this.Enabled = true;
+        }
+
+        private void sendFiles()
+        {
+            if (FilesListBox.Length != 0)
+            {
+                for (int i = 0; i < FilesListBox.Length; i++)
+                {
+                    List.AddFileToList(FilesListBox[i]);
+                }
+
+                History.AppendText(DateTime.Now.ToLongTimeString() + "  " + Name + ": Send these files to server: ");
+                for (int i = 0; i < FilesListBox.Length; i++)
+                {
+                    History.AppendText(FilesListBox[i]);
+                    string file = System.Convert.ToBase64String(System.IO.File.ReadAllBytes(FilesListBox[i]));
+                    string filename = FilesListBox[i].Split('\\')[FilesListBox[i].Split('\\').Length - 1];
+                    String messg = JsonConvert.SerializeObject(new ChatMessage("File", Name, filename, DateTime.Now.ToLongTimeString(), file));
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes(messg);
+                    sendBytes(data);
+                }
+                History.AppendText("\n");
+            }
+        }
+
+        private void sendBytes(byte[] bytes)
+        {
+            try
+            {
+                while (client.Connected == false)
+                {
+                    client = new TcpClient();
+                    client.Connect(SERVER_IP, SERVER_PORT);
+                }
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Flush();
+
+                byte[] data = System.Text.Encoding.UTF8.GetBytes("END");
+                stream.Write(data, 0, data.Length);
+                stream.Flush();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException: {0}", ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+
+        private byte[] recieveBytes()
+        {
+            return null;
         }
     }
 }
